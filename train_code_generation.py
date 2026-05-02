96b6eaecdf5d5087c8dacb41856e118d468bab65
kroos17605-a11y <kroos17605@gmail.com>
2026-05-03 01:41:31 +0800

    Add files via upload

---
+++ train_code_generation.py
@@ -0,0 +1,18 @@
+
+out_dir = 'out-code-gen'
+eval_interval = 250
+eval_iters = 20
+log_interval = 50
+always_save_checkpoint = False
+dataset = 'code_generation'
+batch_size = 12
+block_size = 64
+n_layer = 4
+n_head = 4
+n_embd = 128
+dropout = 0.0
+learning_rate = 1e-3
+max_iters = 1000
+lr_decay_iters = 1000
+min_lr = 1e-4
+warmup_iters = 50
